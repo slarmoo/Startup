0021b4a8d4af81e8fb59024d46e144a31b5a5408
Matthew Medford <matthew.s.medford@gmail.com>
2024-02-26 10:02:38 -0700

    added create account js

--- create.js
+++ create.js
@@ -1,1 +1,24 @@
 //create
+function create() {
+    const nameEl = document.querySelector("#username");
+    const passEl = document.querySelector("#password");
+    const birthEl = document.querySelector("#birth");
+    const parsedDate = birthEl.value.split("-");
+    const confirmEl = document.querySelector("#comfirm");
+    if(passEl.value && passEl.value != "") {
+        if(passEl.value === confirmEl.value) {
+            if(parsedDate[0] <= 2011) {
+                localStorage.setItem("userName", nameEl.value);
+                window.location.href = "browse.html";
+            } else {
+                document.querySelector("#feedback").innerHTML = "You aren't old enough";
+            }
+        } else {
+            document.querySelector("#feedback").innerHTML = "Double check confirm password";
+        }
+    } else {
+        document.querySelector("#feedback").innerHTML = "Make sure you enter a password";
+    }
+}
+    
+  
