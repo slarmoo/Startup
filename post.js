42acc43d56e859a7dabb20ecd2cffa0688793e9a
Matthew Medford <matthew.s.medford@gmail.com>
2024-02-26 10:21:30 -0700

    added username display

--- post.js
+++ post.js
@@ -1,1 +1,11 @@
 //post
+let userName = localStorage.getItem("userName");
+userName = uppercase(userName);
+document.querySelector("#userName").innerHTML = userName;
+function uppercase(word) {
+    let newWord = word[0].toUpperCase();
+    for(let i = 1; i < word.length; i++) {
+        newWord = newWord + word[i];
+    }
+    return newWord;
+}
--- settings.js
+++ settings.js
@@ -1,1 +1,11 @@
 //settings
+let userName = localStorage.getItem("userName");
+userName = uppercase(userName);
+document.querySelector("#userName").innerHTML = userName;
+function uppercase(word) {
+    let newWord = word[0].toUpperCase();
+    for(let i = 1; i < word.length; i++) {
+        newWord = newWord + word[i];
+    }
+    return newWord;
+}
