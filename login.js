fdffbda74c358b2c5c56fb2d52c910b05955564b
Matthew Medford <matthew.s.medford@gmail.com>
2024-02-26 09:26:55 -0700

    added login functionality

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
 //login
+function login() {
+    const nameEl = document.querySelector("#username");
+    const passEl = document.querySelector("#password");
+    if(passEl.value && passEl.value != "") {
+        localStorage.setItem("userName", nameEl.value);
+        window.location.href = "browse.html";
+      }
+    else {
+        document.querySelector("#welcome").innerHTML = "Make sure you enter a password";
+    }
+}
+    
+  
