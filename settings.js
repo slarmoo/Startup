a30f3876fdf72ecab8473122999b673b3393e33e
Matthew Medford <matthew.s.medford@gmail.com>
2024-02-27 12:10:41 -0700

    started access day functionality

--- settings.js
+++ settings.js
@@ -9,3 +9,23 @@ function uppercase(word) {
     }
     return newWord;
 }
+let themeEl = document.querySelector("#theme");
+localStorage.setItem("theme", themeEl.value);
+setDay();
+function setDay() {
+    let dayEl = document.querySelector("#day");
+    localStorage.setItem("day", dayEl.value);
+    checkDay();
+}
+function checkDay() {
+    let d = new Date();
+    let day = d.getDay();
+    const dayParser = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    if(dayParser[day] != localStorage.getItem("day")) {
+        mainEl = document.querySelector(".bulk");
+        while (mainEl.firstChild) {
+            mainEl.firstChild.remove();
+        }
+        mainEl.innerHTML = "Unable to Access on " + dayParser[day] + "s";
+    }
+}
