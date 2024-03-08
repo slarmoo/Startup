7e907cbdc5326bb9806d1ff5a56e5151712cd01c
slarmoo <matthew.s.medford@gmail.com>
2024-03-08 10:10:05 -0700

    began webservice stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 4000; 
+// // The service port. In production the front-end code is statically hosted by the service on the same port.
+// const port = process.argv.length > 2 ? process.argv[2] : 4000; 
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
@@ -13,3 +13,5 @@ app.use(express.static('public'));
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+app.listen(4000)
