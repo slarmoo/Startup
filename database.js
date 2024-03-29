4f92b59d1b9ebb477d80f2ed212e8525d193ffa2
slarmoo <matthew.s.medford@gmail.com>
2024-03-29 13:17:29 -0600

    worked on login

--- database.js
+++ database.js
@@ -1,31 +1,59 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+
+const uuid = require('uuid');
+
 
 const config = require('./dbConfig.json');
 const url = 'mongodb+srv://' + config.userName + ':' + config.password + '@' + config.hostname;
 const client = new MongoClient(url);
 
-//test
-const db = client.db('rental');
-const collection = db.collection('house');
+const postCollection = client.db('startup').collection('posts');
+const userCollection = client.db('startup').collection('users');
 
 (async function testConnection() {
 await client.connect();
-await db.command({ ping: 1 });
+await client.db('startup').command({ ping: 1 });
 })().catch((ex) => {
 console.log(`Unable to connect to database with ${url} because ${ex.message}`);
 process.exit(1);
 });
 
+//posts
 async function getPosts() {
     const query = {  };
     const options = {
-        sort: { score: -1 },
         limit: 10,
     };
 
-    const cursor = collection.find(query, options);
+    const cursor = postCollection.find(query, options);
     const posts = await cursor.toArray();
     return posts;
 }
 
-module.exports = { getPosts };
+async function addPost(text, image) {
+    const newPost = {
+        text: text,
+        image: image,
+    };
+    postCollection.insertOne(newPost);
+}
+
+//login
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+  }
+
+module.exports = { getPosts, addPost, getUser, createUser };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const bcrypt = require('bcrypt');
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
@@ -28,11 +29,13 @@ let data;
 
 apiRouter.post("/post", (req, res)=>{
     data = req.body;
+    console.log(data);
+    DB.addPost();
 });
 
 apiRouter.get("/post", (req, res)=>{
-    console.log("data = "+data);
     data = DB.getPosts();
+    console.log("data = "+data);
     if(data) {
         res.send(data);
     } else {
@@ -43,6 +46,55 @@ apiRouter.get("/post", (req, res)=>{
     }
 });
 
+//login
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await DB.createUser(req.body.email, req.body.password);
+
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id: user._id,
+        });
+    }
+  });
+
+  function setAuthCookie(res, authToken) {
+    res.cookie('token', authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+
+  app.post('/auth/login', async (req, res) => {
+    const user = await getUser(req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+
+  app.get('/user/me', async (req, res) => {
+    authToken = req.cookies['token'];
+    const user = await collection.findOne({ token: authToken });
+    if (user) {
+      res.send({ email: user.email });
+      return;
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+
+//final
 app.use(`/api`, apiRouter);
 
 app.listen(4000)
