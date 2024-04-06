20b9c845a19a9018387b1dcb726a0ed4177d1e9f
slarmoo <matthew.s.medford@gmail.com>
2024-04-05 18:41:56 -0600

    began implementing peerProxy file with the help of Simon

--- peerProxy.js
+++ peerProxy.js
@@ -1,2 +1,38 @@
 const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
+function peerProxy(httpServer) {
+    const wss = new WebSocketServer({ port: 9900 });
+
+    httpServer.on("upgrade", (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit("commection", ws, request);
+        });
+    });
+
+    let connections = [];
+
+    wss.on('connection', (ws) => {
+        const connection = { id: uuid.v4(), alive: true, ws: ws };
+        connections.push(connection);
+
+        ws.on('message', (data) => {
+            connections.forEach((c) => {
+                if(c.id !== connection.id) {
+                    c.ws.send(data);
+                }
+            });
+        });
+
+
+        ws.on("close", () => {
+            const pos = connections.findIndex((o, i) => i.id === connection.id);
+
+            if (pos >= 0) {
+                connections.splice(pos, 1);
+            }
+        })
+    });
+
+}
+
+module.exports = { peerProxy }
