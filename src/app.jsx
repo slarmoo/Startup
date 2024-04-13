3fcc4cabdd98acab570a224b1f3ea9f45ef41ac6
slarmoo <matthew.s.medford@gmail.com>
2024-04-12 18:54:09 -0600

    post now properly working

--- src/app.jsx
+++ src/app.jsx
@@ -14,6 +14,42 @@ function App() {
     const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
     const [authState, setAuthState] = React.useState(currentAuthState);
 
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    configureWebSocket();
+    let notifs = [];
+
+    async function deleteCookie() {
+        const del = await fetch("/user/expire", {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                "credentials": "include"
+            },
+        });
+    }
+    
+    function configureWebSocket() {
+        socket.onopen = (event) => {
+            displayMsg('system', 'websocket', 'connected');
+        };
+        socket.onclose = (event) => {
+            displayMsg('system', 'websocket', 'disconnected');
+        };
+        socket.onmessage = async (event) => {
+            const msg = JSON.parse(await event.data.text());
+            displayMsg('user', msg.from, "made a post!");
+        };
+        socket.onerror = (error) => {
+            console.error('WebSocket error:', error);
+        };
+    }
+
+    function displayMsg(clss, from, msg) {
+        console.log(notifs);
+        notifs.push(`<div class="event"><span class="${clss}Event">${from}</span> ${msg}</div>`);
+    }
+
     return (
         <BrowserRouter>
             <div className="body">
@@ -29,9 +65,9 @@ function App() {
                             <li className="text1"><NavLink to="browse" className="link">Browse</NavLink></li>
                             <li className="text1"><NavLink to="post" className="link">Post</NavLink></li>
                             <li className="text1"><NavLink to="settings" className="link">Settings</NavLink></li>
-                            <li className="text1"><NavLink onClick="deleteCookie()" to="/" className="link">Logout</NavLink></li>
+                            <li className="text1"><NavLink onClick={() => deleteCookie()} to="/" className="link">Logout</NavLink></li>
                         </menu>
-                        <div className="notif"></div>
+                        <div className="notif">{notifs}</div>
                     </nav>
 
                         <Routes>
@@ -48,9 +84,9 @@ function App() {
                                 }
                                 exact
                             />
-                            <Route path='/create' element={<Create userName={userName} />} />
+                            <Route path='/create' element={<Create userName={userName} socket={socket} />} />
                             <Route path='/browse' element={<Browse userName={userName} />} />
-                            <Route path='/post' element={<Post userName={userName} />} />
+                            <Route path='/post' element={<Post userName={userName} socket={socket} />} />
                             <Route path='/settings' element={<Settings userName={userName} />} />
                             <Route path='*' element={<NotFound />} />
                         </Routes>
---
+++ src/post/mainStuff.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+
+export function MainStuff(above) {
+    const [postText, setPostText] = React.useState("");
+    const [previewSource, setPreviewSource] = React.useState("");
+    const[error, setError] = React.useState(false);
+    // React.useEffect(() => {
+    //     console.log(previewSource);
+    // }, [previewSource]);
+
+    async function preview(e) {
+        let file = e;
+        const img = new Image();
+        let imggg;
+        img.src = URL.createObjectURL(file);
+
+        img.onload = async function () {
+            try {
+                imggg = await compressImage(img, 0.15);
+                setError(false);
+                setPreviewSource(imggg);
+            } catch {
+                setError(true);
+                console.log("ERROR");
+            }
+        }
+    }
+
+    async function savePost() {
+        console.log(postText, previewSource);
+        if (postText != "" && previewSource != "") {
+            console.log("post1")
+            const img = new Image();
+            let d = new Date();
+            let day = d.getDate();
+            if (day.length < 2) {
+                day = "0" + day.toString();
+            }
+            let month = d.getMonth();
+            let date = month.toString() + day.toString();
+            date = Number(date);
+
+            console.log("post2")
+            let username = await fetch("/user/me", {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    "credentials": "include"
+                },
+            });
+            const userData = await username.json();
+            const userName = userData.username;
+            const delta = { text: postText, image: previewSource, date: date, user: userName };
+            console.log("post3")
+
+            const newPost = fetch("/api/post", {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(delta),
+            });
+            setPreviewSource("");
+            setPostText("");
+            if (above.socket.readyState === WebSocket.OPEN) {
+                broadcastEvent(userName);
+            } else {
+                console.warn('WebSocket not open yet.');
+            }
+        }
+    }
+
+    function compressImage(image, i) {
+        return new Promise((resolve, reject) => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+            const compressedDataURL = canvas.toDataURL('image/jpeg', i);
+            if (compressedDataURL.length <= 9000) {
+                resolve(compressedDataURL);
+            } else {
+                const img = new Image();
+                img.src = compressedDataURL;
+                img.onload = function () {
+                    if (i > 0.1) {
+                        resolve(compressImage(image, i - 0.05));
+                    } else if (i > 0.001) {
+                        resolve(compressImage(image, i / 2));
+                    } else {
+                        reject(new Error('Image compression failed'));
+                    }
+
+                }
+            }
+        });
+    }
+
+    function broadcastEvent(from) {
+        const event = {
+            from: from,
+        };
+       above.socket.send(JSON.stringify(event));
+    }
+
+    let d = new Date();
+    let day = d.getDay();
+    const dayParser = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let access = localStorage.getItem("day");
+    if (!access) {
+        access = "Friday";
+    }
+    if (dayParser[day] !== access) {
+        return <div style={{ justifySelf: "center", alignSelf: "center" }} id="postBulk">Unable to access website today. Try again on {access}.</div>;
+    } else if (error) { 
+        return (<div className="text1" id="postBulk">
+            <textarea id="postText" value={postText} onChange={(e) => setPostText(e.target.value)}></textarea>
+            <div>
+                <input id="postImg" type="file" accept=".jpeg,.png,.tiff,.jpg" className="text1" onChange={(e) => preview(e.target.files[0])} />
+                <button onClick={() => savePost()} className="text1" id="button">Post</button>
+            </div>
+            <div>
+                <img src="" width="150" alt="your image here!" id="preview" />
+            </div>
+            <p>File too large</p>
+        </div>)
+    } else {
+        return (
+            <div className="text1" id="postBulk">
+                <textarea id="postText" value={postText} onChange={(e) => setPostText(e.target.value)}></textarea>
+                <div>
+                    <input id="postImg" type="file" accept=".jpeg,.png,.tiff,.jpg" className="text1" onChange={(e) => preview(e.target.files[0])} />
+                    <button onClick={() => savePost()} className="text1" id="button">Post</button>
+                </div>
+                <div>
+                    <img src={previewSource} width="150" alt="your image here!" id="preview" />
+                </div>
+            </div>
+        )
+    }    
+}
